2232d35c8f2a7c26a769be545a901b0ad996cc5a
oribe(oribe1115) <50443000+oribe1115@users.noreply.github.com>
2022-07-19 22:43:23 +0900

    非描画モードの時は描画を無視して操作できるようにした

--- app/src/main/java/work/oribe/paintover/OverlayView.kt
+++ app/src/main/java/work/oribe/paintover/OverlayView.kt
@@ -118,6 +118,8 @@ class OverlayView(ctx: Context, attrs: AttributeSet) :
         return WindowManager.LayoutParams(
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                     or WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
             PixelFormat.TRANSLUCENT
         )
